b8e2e1f0bea8f91508b77d3cb92e88b5d40e4600
Yong Lian Hii <Hii@Yongs-MacBook-Pro.local>
2015-07-26 14:44:01 +0800

    Added key value storage for mmls passwords

--- mmls_downloader.rb
+++ mmls_downloader.rb
@@ -1,38 +1,49 @@
 require 'mechanize'
 require "highline/import"
+require 'daybreak'
+
+db = Daybreak::DB.new "mmls.db"
 agent = Mechanize.new
 agent.agent.http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 page = agent.get("https://mmls.mmu.edu.my")
 form = page.form
-
 print "               #######################################\n"
 print "               |         MMLS      DOWNLOADER        |\n"
 print "               |               BY                    |\n"
 print "               |           HII YONG LIAN             |\n"
 print "               #######################################\n"
-loop do
-  student_id = ask "Input Student ID: "
-  mmls_password = ask("Input MMLS password (Input will be hidden): ") { |q| q.echo = false }
-
-  form.stud_id = student_id# INPUT YOUR STUDENT ID
-  form.stud_pswrd = mmls_password# INPUT YOUR MMLS PASSWORD
+if db.keys.include? 'mmls_password'
+  print "            ----------    Loaded MMLS Password --------  "
+  form.stud_id = db['student_id']# INPUT YOUR STUDENT ID
+  form.stud_pswrd = db['mmls_password']# INPUT YOUR MMLS PASSWORD
   page = agent.submit(form)
-  break if page.parser.xpath('//*[@id="alert"]').empty?
-  retry_reply = ask("Student ID or password is invalid... Would you like to retry? (Y/N)")
+else
   loop do
-    if(retry_reply == 'Y' or retry_reply == 'y')
+    student_id = ask "Input Student ID: "
+    mmls_password = ask("Input MMLS password (Input will be hidden): ") { |q| q.echo = false }
+
+    form.stud_id = student_id# INPUT YOUR STUDENT ID
+    form.stud_pswrd = mmls_password# INPUT YOUR MMLS PASSWORD
+    page = agent.submit(form)
+    if page.parser.xpath('//*[@id="alert"]').empty?
+      db.set! 'student_id', student_id
+      db.set! 'mmls_password', mmls_password
       break
-    elsif(retry_reply == 'N' or retry_reply == 'n')
-      exit
-    else
-      retry_reply = ask("Unrecognized input... Would you like to retry? (Yy/Nn)")
+    end
+    retry_reply = ask("Student ID or password is invalid... Would you like to retry? (Y/N)")
+    loop do
+      if(retry_reply == 'Y' or retry_reply == 'y')
+        break
+      elsif(retry_reply == 'N' or retry_reply == 'n')
+        db.close
+        exit
+      else
+        retry_reply = ask("Unrecognized input... Would you like to retry? (Yy/Nn)")
+      end
     end
   end
-
-
 end
-
-
+db.close
 agent.pluggable_parser.default = Mechanize::Download
 subject_links = page.links_with(:text => /[A-Z][A-Z][A-Z][0-9][0-9][0-9][0-9] . [A-Z][A-Z][A-Z]/)
 subject_links.each do |link|
